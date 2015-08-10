5e4e159ca5523c15179b575f182595715c355ac6
Piiit <pitiz29a@gmail.com>
2015-08-10 19:43:42 +0200

    Update pw_make.sh

--- pw_make.sh
+++ pw_make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm /tmp/postgresql-temporal2-serverlog
+rm /tmp/postgresql-temporal3-serverlog
 make && make install && pw_pgcontrol.sh restart && sleep 3 && clear && tail -f /tmp/postgresql-temporal3-serverlog
