4f566130319bccf2934ba11daea32bcd2a1e7bc7
Piiit <pitiz29a@gmail.com>
2015-08-13 16:51:02 +0200

    pw_make for postgres development no longer waits 3 seconds, but until log file exists => faster

--- pw_make.sh
+++ pw_make.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-rm /tmp/postgresql-temporal3-serverlog
-make && make install && pw_pgcontrol.sh restart && sleep 3 && clear && tail -f /tmp/postgresql-temporal3-serverlog
+LOG=/tmp/postgresql-temporal3-serverlog
+
+rm $LOG
+make && make install && pw_pgcontrol.sh restart 
+
+# Wait until log file exists, i.e. server started...
+while [ ! -f $LOG ]
+do
+	echo -n .
+done
+
+clear
+tail -f $LOG
