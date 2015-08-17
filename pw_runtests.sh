e89e8e5f5c184becadbb1682248f707a4aa3b20d
Piiit <pitiz29a@gmail.com>
2015-08-17 17:07:47 +0200

    Runtests refactored

--- pw_runtests.sh
+++ pw_runtests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+SCRIPTNAME=${0##*/}
+
 function showHelp {
-	echo "Command: $0 [OPTIONS]... [FILES]..."
+	echo "Command: $SCRIPTNAME [OPTIONS]... [FILES]..."
 	echo "Runs test-cases of given [FILES], compares the output of a method with an expected-output file."
 	echo "Returns SUCCEEDED for outputs that are equal to the expected file content."
 	echo "Needed: A input file (<something>.txt) and an expected output file (<something>_expected.txt),"
@@ -13,42 +15,77 @@ function showHelp {
 	echo "  -i, --ignore <pattern>	Ignore lines with a certain <pattern>"
 }
 
-OPTIND=1
-VERBOSE=0
-IGNORE=0
+# NOTE: This requires GNU getopt.  On Mac OS X and FreeBSD, you have to install this
+# separately; see below.
+TEMP=$(getopt -o hvic: --long help,verbose,ignore,command:,debugfile: -n $SCRIPTNAME -- "$@")
 
-function outVerbose {
-	test $VERBOSE -eq 1 && echo "$1" >&2
-}
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 
-while getopts "hvi" key
-do
-	case $key in
-		h)
-			showHelp	
-			exit 0
-		;;
-		v)
-			VERBOSE=1
-		;;
-		i)
-			IGNORE=1
-			IGNORESTRING="$2"
-			shift $((OPTIND-1))
-		;;
-		*)
-			echo "'$0 -h' gives you more information."
-			exit 1
-		;;
-	esac
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$TEMP"
+
+VERBOSE=false
+IGNORE=false
+DEBUGFILE=
+CMD=
+
+while true; do
+  case "$1" in
+    -h | --help ) 
+    	showHelp
+    	exit 0 
+    ;;
+    -v | --verbose ) 
+    	VERBOSE=true
+    	shift 
+    ;;
+    -i | --ignore ) 
+    	IGNORE=true
+		IGNORESTRING="$2" 
+		shift 2 
+	;;
+	-c | --command )
+		CMD="$2"
+		shift 2
+	;;
+    --debugfile ) 
+    	DEBUGFILE="$2"
+    	shift 2 
+    ;;
+    -- ) 
+    	shift
+    	break 
+    ;;
+    * ) 
+    	echo "'$SCRIPTNAME -h' gives you more information."
+		exit 1
+	;;
+  esac
 done
 
-shift $((OPTIND-1))
-[ "$1" = "--" ] && shift
+function outVerbose {
+	$VERBOSE && echo "$1" >&2
+}
 
 ERRCOUNT=0
 SKIPCOUNT=0
 TESTCOUNT=0
+
+function diffCmd {
+
+	DIFFCMD="diff -Bbc --suppress-common-lines "
+	if $IGNORE; then
+		DIFFCMD=$DIFFCMD"-I $IGNORESTRING "
+	fi
+	
+	if test -n "$CMD"; then
+		$CMD $2 | $DIFFCMD $1 -
+		return $?
+	fi
+	
+	$DIFFCMD $1 $2
+	return $?
+}
 
 for ARG in "$@"
 do
@@ -58,12 +95,12 @@ do
 			outVerbose "============================================================================="
 			outVerbose "TESTING: $ARG"
 			
-			if test $IGNORE -eq 1; then
+			if $IGNORE; then
 				outVerbose "Ignoring '$IGNORESTRING' while comparing..."
 				diff -Bby --suppress-common-lines -I $IGNORESTRING ${ARG%.*}_expected.txt "$ARG" >&2
                 PASSED=$?
 			else
-				diff -Bby --suppress-common-lines ${ARG%.*}_expected.txt "$ARG" >&2
+				diffCmd ${ARG%.*}_expected.txt "$ARG"  >&2
                 PASSED=$?
 			fi
 			
