8a1cbf5db7ce87c461e6615caa10a22a4c8118c5
Piiit <pitiz29a@gmail.com>
2015-10-23 16:36:29 +0200

    pw_make makes additional checks on input and files, shows correct errors now

--- pw_make.sh
+++ pw_make.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-
+SCRIPTNAME=${0##*/}
 INI=.pw_pgcontrol.ini
 
+function showError {
+    echo "$SCRIPTNAME: ERROR: $1"
+	echo
+}
+
 test -f $INI && . $INI || { 
-		showError "No evironment INI file found. Have you specified PostgreSQL configs in $INI?"
-		exit 1
-	}
-	
-rm $LOGFILE
+	showError "No evironment INI file found. Have you specified PostgreSQL configs in $INI?"
+	exit 1
+}
+
+echo $LOG
+
+test -z $LOG && {
+	showError "LOG not set in INI file $INI."
+	exit 1
+}
+
+rm -f $LOG || {
+	showError "Can not remove LOG file $LOG."
+	exit 1
+}
+
 make && make install && pw_pgcontrol.sh --restart 
 
 if test $? -ne 0; then
@@ -15,12 +31,12 @@ if test $? -ne 0; then
 fi
 
 # Wait until log file exists, i.e. server started...
-while [ ! -f $LOGFILE ]
+while [ ! -f $LOG ]
 do
 	echo -n -e "\rWaiting for log file..."
 done
 
 clear
-tail -f $LOGFILE
+tail -f $LOG
 
 exit 0
